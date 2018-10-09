3fd34660bcb162813cc2da768facd4711febe920
leejend <jeanettelee09@optusnet.com.au>
2018-10-09 21:17:13 +1100

    tutorial 8 q4 complete

--- Tutorial 8_Q4/ViewController.swift
+++ Tutorial 8_Q4/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var switch2: UISwitch!
+    @IBOutlet weak var myText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func showSwitchedStatus(_ sender: Any) {
+        //get the name from text field
+        let name = myText.text!
+       
+        
+        //get the switch status
+        var msg:String;
+        if switch2.isOn {
+            msg =  " Female"
+        }
+        else {
+            msg = " Male"
+        }
+    
+    //create an UIAlert Message
+        let alert = UIAlertController(title: "Hello", message: name + msg, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction (UIAlertAction (title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    self.present(alert, animated: true, completion: nil)
+}
 }
 
